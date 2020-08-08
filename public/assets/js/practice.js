44398c8028c9138ddb32def176fdca315db6c1dd
kugimasa <kugimania@gmail.com>
2020-08-08 23:36:31 +0900

    Update toggle switch

--- public/assets/js/practice.js
+++ public/assets/js/practice.js
@@ -1,41 +1,37 @@
 // 練習ボタンを押したとき
-var practice = function() {
-		var stopImageNumber = decideFacialExpression();
-		if(document.getElementById("switch").checked){
-			playRoulette(stopImageNumber);
-			playDrumroll();
-		}else{
-			setTimeout( () => countDown(), 500);
-		}
+var practice = function () {
+  var stopImageNumber = decideFacialExpression();
+  if (document.getElementById("switch").checked) {
+    playRoulette(stopImageNumber);
+    playDrumroll();
+  } else {
+    setTimeout(() => countDown(), 500);
+  }
 };
 
-var playRoulette = function(stopImageNumber){
-	var p = {
-		startCallback : function() {
-		},
-		slowDownCallback : function() {
-		},
-		stopCallback : function() {
-			showFaceText(stopImageNumber);
-			setTimeout( () => countDown(), 3000);
-		},
-		stopImageNumber : stopImageNumber
-	}
-	var rouletter = $('div.roulette');
-	rouletter.roulette(p);
-  rouletter.roulette('start');
-}
+var playRoulette = function (stopImageNumber) {
+  var p = {
+    startCallback: function () {},
+    slowDownCallback: function () {},
+    stopCallback: function () {
+      showFaceText(stopImageNumber);
+      setTimeout(() => countDown(), 3000);
+    },
+    stopImageNumber: stopImageNumber,
+  };
+  var rouletter = $("div.roulette");
+  rouletter.roulette(p);
+  rouletter.roulette("start");
+};
 
-var toggleBattleContainer = function() {
-  // Get the checkbox
-  var checkBox = document.getElementById("switch");
-  // Get the output text
-  var battleContainer = document.getElementById("battle-container");
-
-  // ここからしたはかえてちょ
-  if (checkBox.checked == true){
-    battleContainer.style.display = "block";
-  } else {
-    battleContainer.style.display = "none";
-  }
-}
+// ToggleBattleContainer
+$(function() {
+	$('#switch').change(function() {
+		var battleContainer = document.getElementById("battle-container");
+		if ($(this).prop('checked')) {
+			battleContainer.style.display = "block";
+		} else {
+			battleContainer.style.display = "none";
+		}
+	})
+})
